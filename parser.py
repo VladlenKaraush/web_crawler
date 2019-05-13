4fafe161cf3143bf2d26e1335ec2565653832de0
Vladlen <vladlenkaraush@gmail.com>
2019-05-13 21:45:30 +0300

    uncomment & write to file added

--- parser.py
+++ parser.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
+# In[84]:
 
 
 # url to first pages
@@ -16,12 +16,15 @@ city = "Санкт-Петербург"
 # cian_url = 'https://samara.cian.ru/cat.php?deal_type=sale&engine_version=2&offer_type=flat&region=4966&room1=1&room2=1&room3=1&room4=1&room5=1&room6=1&room9=1'
 # city = 'Самара'
 
-csv_dir = 'csv-data'
 api_key = '800e1c1a-3d68-496e-88a9-009b98d7eb75'
+api_keys = ['800e1c1a-3d68-496e-88a9-009b98d7eb75', 'c7bc2c4c-0414-4156-8bb3-bc12e8b38baa', 'c1ddf482-774f-4196-b40f-e1fcca928f34', '0b2344b3-fca3-4d27-a13e-5de997a53a4a']
 api_url = 'https://geocode-maps.yandex.ru/1.x/?apikey=' + api_key + '&format=json&geocode=' + city + ',+'
+api_urls = [f'https://geocode-maps.yandex.ru/1.x/?apikey={api_key}&format=json&geocode=' + city + ',+' for api_key in api_keys]
+tagged = {}
+keys_number = len(api_urls)    
 
 
-# In[5]:
+# In[86]:
 
 
 # execute this cell to load all neccessary functions
@@ -33,11 +36,56 @@ import re
 
 #write collected data to .csv file as a pandas dataframe
 import numpy
-import pandas as pd 
+import pandas as pd
 
 #decode geotagging answer
 import json
 import csv
+
+import time
+import multiprocessing
+from multiprocessing import Pool
+from multiprocessing.dummy import Pool as ThreadPool
+
+
+
+def write_to_csv_tagged(new_data_flats, file_name):
+    a = numpy.asarray(new_data_flats)
+    pd.DataFrame(a).to_csv(file_name, header=['price', 'rooms', 'area', 'floor', 'floors_total', 'adress',
+                                              'subway_station', 'distance to subway', 'longitude', 'latitude'])
+    
+def geotag_single_row(row):
+    ind = row[0]
+    row = row[1]
+    print('requested address = ', row[6].replace(' ', '+'))
+    print('full url = ', api_urls[ind % keys_number] + row[6].replace(' ', '+'), 'ind = ', ind)
+    r = requests.get(api_urls[ind % keys_number] + row[6].replace(' ', '+'))
+    d = json.loads(r.text)
+    if d['response']['GeoObjectCollection']['metaDataProperty']['GeocoderResponseMetaData']['found'] != 0:
+        longitude, latitude =         d['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos'].split()
+        print('long = ', longitude, ', latitude = ', latitude)
+        tagged[ind] = (*row, longitude, latitude)
+    else:
+        print("geoposition not found, row skipped")
+
+#add geotagging to existing csv file
+def geotag(file_in, file_out):
+    
+    with open(file_in) as csv_file, open(file_out, 'w') as out:
+
+        writer = csv.writer(out, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        rows = [(ind,row) for ind, row in enumerate(csv_reader)]
+        
+        pool = ThreadPool()
+        results = pool.map(geotag_single_row, rows)
+        pool.close()
+        pool.join()
+        
+        t_keys = [key for key in tagged.keys()]
+        rows = [(tagged[key][1],tagged[key][2],tagged[key][3],tagged[key][4],tagged[key][5],tagged[key][6],tagged[key][7],tagged[key][8],tagged[key][9],tagged[key][10]) for key in t_keys ]
+        for row in rows:
+            writer.writerow(row)
 
 #avito
 #extract price
@@ -52,6 +100,8 @@ def price_from_json(flat):
     price_in_rub = price_cut.split(",")[0]
     return int(price_in_rub)
 
+
+
 #extract all relevant data from single page
 def parse_page_avito(flats, data_flats):
     rent_count = 0
@@ -63,13 +113,12 @@ def parse_page_avito(flats, data_flats):
         #price
         print("price:")
         price = price_from_json(flat)
-        if(price < 300_000):
+        if (price < 300_000):
             rent_count += 1
             print("Rent found, skip. price = ", price)
             continue
         print(price)
         print()
-
 
         #rooms, area, floor level
         print("rooms:")
@@ -105,7 +154,7 @@ def parse_page_avito(flats, data_flats):
         print()
 
         subway = full_adress.split(",")[0]
-        for (ind,el) in enumerate(subway.split()):
+        for (ind, el) in enumerate(subway.split()):
             try:
                 float(el)
                 subway_station = ' '.join(subway.split()[:ind])
@@ -125,17 +174,18 @@ def parse_page_avito(flats, data_flats):
 
         print("go to next page")
         data_flats.append((price, rooms, area, floor, floors_total, adress, subway_station, distance))
-    
+
     print(rent_count)
     return data_flats
 
 
 def parse_avito(avito_url):
+
     avito_real_estate = urllib.request.urlopen(avito_url).read()
     #parse avito with beautiful soup
     avito_soup = BeautifulSoup(avito_real_estate)
     # url_base = "https://www.avito.ru"
-    flats = avito_soup.findAll("div", {"class":"item_table-description"})
+    flats = avito_soup.findAll("div", {"class": "item_table-description"})
     page_count = 0
 
     data_flats = parse_page_avito(flats, [])
@@ -144,14 +194,14 @@ def parse_avito(avito_url):
         try:
             page_count += 1
             print("Page: ", page_count)
-            next_page = avito_soup.find("a", {"class":"pagination-page"}).get('href')
-            flats = avito_soup.findAll("div", {"class":"item_table-description"})
+            next_page = avito_soup.find("a", {"class": "pagination-page"}).get('href')
+            flats = avito_soup.findAll("div", {"class": "item_table-description"})
             data_flats = parse_page_avito(flats, data_flats)
 
             if page_count > 100:
                 print('page count > 100, break')
                 break
-        except Exception as error:
+        except Error as error:
             print(error, ' error, break')
             break
 
@@ -159,19 +209,46 @@ def parse_avito(avito_url):
     print("data len: ", len(data_flats))
     new_data_flats = []
     for flat in data_flats:
-        new_flat = (flat[0],flat[1],flat[2],flat[3],flat[4],flat[5].replace('\xa0', '', 2),flat[6],flat[7])
+        new_flat = (flat[0], flat[1], flat[2], flat[3], flat[4], flat[5].replace('\xa0', '', 2), flat[6], flat[7])
         new_data_flats.append(new_flat)
-    
+
     return new_data_flats
 
+
+flat_map = {}
+def avito_thread(url):
+    flats = parse_avito(url)
+    flat_map[url] = flats
+    print('finished parsing url = ', url, ', len = ', len(flats))
+
 def write_to_csv(new_data_flats, file_name):
-    
     a = numpy.asarray(new_data_flats)
-    pd.DataFrame(a).to_csv(csv_dir + '/' + file_name, header=['price', 'rooms', 'area', 'floor', 'floors_total', 'adress',
-                                           'subway_station', 'distance to subway'])
+    pd.DataFrame(a).to_csv(file_name, header=['price', 'rooms', 'area', 'floor', 'floors_total', 'adress',
+                                              'subway_station', 'distance to subway'])
+    
+def avito_concurrent(out_file):
+    
+    rooms = [f'{i}-komnatnye' for i in range(1,4)]
+    price_grade = [0, 3_000_000, 6_000_000, 10_000_000, 20_000_000, 100_000_000]
+    price_ranges = [f'pmax={price_grade[i + 1]}&pmin={price_grade[i]}' for i in range(len(price_grade) - 1)]
+    urls = []
+    for room in rooms:
+        for price in price_ranges:
+            urls.append(f'https://www.avito.ru/sankt-peterburg/kvartiry/prodam/{room}?{price}&s_trg=4')
+    start = time.time()
+    pool = ThreadPool()
+    results = pool.map(avito_thread, urls)
+    pool.close()
+    pool.join()
+    print('total finish, time = ', time.time() - start, ', data size = ', len(flat_map))
+    
+    for key in flat_map.keys():
+        flats.extend(flat_map[key])
+    write_to_csv(flats, out_file)
+
 
 #cian
-    
+
 def next_page_cian(first_page, soup):
     next_p = soup.find("li", {"class": re.compile('^.*list-item--active.*')})
     if next_p and next_p.nextSibling:
@@ -182,9 +259,9 @@ def next_page_cian(first_page, soup):
 def parse_cian_page(cian_soup, data_flats):
     counter = 0
     flats = cian_soup.findAll("div", {"class": "c6e8ba5398--info-container--YhZ2y"})
-    print('flats = ',len(flats))
+    print('flats = ', len(flats))
     for flat in flats:
-        
+
         counter += 1
         # rooms, area, floor
         addr = flat.find("div", {"class": re.compile('^.*title.*')})
@@ -197,7 +274,7 @@ def parse_cian_page(cian_soup, data_flats):
             try:
                 area = float(addr.text.split(',')[1].split()[0])
             except Exception as error:
-                print(error,', area error, area = ', addr.text.split(',')[1].split()[0])
+                print(error, ', area error, area = ', addr.text.split(',')[1].split()[0])
                 continue
             print('full addr = ', addr.text)
             print('area = ', area)
@@ -208,7 +285,7 @@ def parse_cian_page(cian_soup, data_flats):
             except Exception as error:
                 print('floors total error = ', error)
             print('floors total = ', floors_total)
-            
+
             print(addr.text)
 
         subway = flat.find("div", {"class": re.compile('^.*underground-name.*')})
@@ -225,19 +302,19 @@ def parse_cian_page(cian_soup, data_flats):
                 remoteness = int(remoteness.text.split('\xa0')[0]) * 100
             else:
                 remoteness = int(remoteness.text.split('\xa0')[0]) * 1000
-            print('distance to subway = ',remoteness)
+            print('distance to subway = ', remoteness)
         else:
             remoteness = None
-            
+
         adress = flat.findAll("a", {"class": re.compile('^.*address.*')})
         if adress:
-            if(len(adress) > 5):
-                adress = adress[4].text+ ', ' + adress[5].text
+            if (len(adress) > 5):
+                adress = adress[4].text + ', ' + adress[5].text
                 print(adress)
             else:
                 continue
-                
-        price = flat.find("div", {"class": re.compile('^.*price.*')}).find("div",{'class': re.compile('.*header.*')})
+
+        price = flat.find("div", {"class": re.compile('^.*price.*')}).find("div", {'class': re.compile('.*header.*')})
         if price:
             print(price.text)
             price_array = price.text.split(' ')[:len(price.text.split(' ')) - 1]
@@ -246,13 +323,14 @@ def parse_cian_page(cian_soup, data_flats):
                 price *= 1000
                 price += int(el)
             print('price = ', price)
-            
+
         data_flats.append((price, rooms, area, floor, floors_total, adress, subway, remoteness))
         print()
     return data_flats
 
 def parse_cian(cian_url):
-    base_url = cian_url.split('cian.ru')[0] + 'cian.ru' 
+    base_url = cian_url.split('cian.ru')[0] + 'cian.ru'
+    print('cian url = ', cian_url)
     cian_real_estate = urllib.request.urlopen(cian_url).read()
     cian_soup = BeautifulSoup(cian_real_estate)
     counter = 0
@@ -260,6 +338,7 @@ def parse_cian(cian_url):
     pages = 58
     while counter < pages:
         counter += 1
+        time.sleep(30) 
         flats = parse_cian_page(cian_soup, flats)
         cian_url = next_page_cian(cian_url, cian_soup)
         if cian_url == -1:
@@ -271,40 +350,45 @@ def parse_cian(cian_url):
         cian_soup = BeautifulSoup(cian_html)
         print("counter = ", counter)
     return flats
-        
-        
-#add geotagging to existing csv file
-def geotag(file_in, file_out):
-    with open(file_in) as csv_file, open(file_out, 'w') as out:
 
-        writer = csv.writer(out, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for ind, row in enumerate(csv_reader):
-            print('row ', ind)
-            if ind == 0:
-                writer.writerow((*row, 'longitude', 'latitude'))
 
-            else:
-                #TODO replace spaces with '+'
-                print('requested address = ', row[6].replace(' ','+'))
-                print('full url = ',api_url + row[6].replace(' ','+') )
-                r = requests.get(api_url + row[6].replace(' ','+'))
-                d = json.loads(r.text)
-                if d['response']['GeoObjectCollection']['metaDataProperty']['GeocoderResponseMetaData']['found'] != 0:
-                    longitude, latitude = d['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos'].split()
-                    print('long = ', longitude, ', latitude = ', latitude)
-                    writer.writerow((*row, longitude, latitude))
-                    out.flush()
-                else:
-                    print("geoposition not found, row skipped")
-                
+# #add geotagging to existing csv file
+# def geotag(file_in, file_out):
+    
+#     api_urls = [f'https://geocode-maps.yandex.ru/1.x/?apikey={api_key}&format=json&geocode=' + city + ',+' for api_key in api_keys]
+#     keys_number = len(api_urls)
+#     with open(file_in) as csv_file, open(file_out, 'w') as out:
+
+        
+#         pool = ThreadPool()
+#         results = pool.map(avito_thread, urls)
+#         writer = csv.writer(out, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+#         csv_reader = csv.reader(csv_file, delimiter=',')
+#         for ind, row in enumerate(csv_reader):
+#             print('row ', ind)
+#             if ind == 0:
+#                 writer.writerow((*row, 'longitude', 'latitude'))
+
+#             else:
+#                 print('requested address = ', row[6].replace(' ', '+'))
+#                 print('full url = ', api_url + row[6].replace(' ', '+'))
+#                 r = requests.get(api_urls[ind % keys_number] + row[6].replace(' ', '+'))
+#                 d = json.loads(r.text)
+#                 if d['response']['GeoObjectCollection']['metaDataProperty']['GeocoderResponseMetaData']['found'] != 0:
+#                     longitude, latitude = \
+#                     d['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos'].split()
+#                     print('long = ', longitude, ', latitude = ', latitude)
+#                     writer.writerow((*row, longitude, latitude))
+#                     out.flush()
+#                 else:
+#                     print("geoposition not found, row skipped")
+
 #merge two data files
 def merge(first, second, out):
-    
     with open(first) as cian, open(second) as avito, open(out, 'w') as out:
         writer = csv.writer(out, delimiter=',', quoting=csv.QUOTE_MINIMAL)
         cian_reader = csv.reader(cian, delimiter=',')
-        avito_reader = csv.reader(avito, delimiter =',')
+        avito_reader = csv.reader(avito, delimiter=',')
 
         for row in avito_reader:
             writer.writerow(row)
@@ -313,7 +397,7 @@ def merge(first, second, out):
             writer.writerow(row)
 
 
-# In[ ]:
+# In[87]:
 
 
 # необходимо сначала исполнить следующую ячейку, чтобы загрузить все функции
@@ -321,8 +405,9 @@ def merge(first, second, out):
 
 # avito to csv
 print('start avito parsing, url = ', avito_url)
-avito_flats = parse_avito(avito_url)
-write_to_csv(avito_flats, 'spb_test_avito.csv')
+avito_concurrent('csv-data/spb_test_avito.csv')
+# avito_flats = parse_avito(avito_url)
+# write_to_csv(avito_flats, 'spb_test_avito.csv')
 print('finished avito parsing')
 
 # cian to csv
